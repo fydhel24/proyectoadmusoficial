8a0325c10576dcfbc78d5753c3f031d7821dd232
fydhel24 <fydhel.24@gmail.com>
2025-12-16 11:51:20 -0400

    commit api

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/auth/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|string',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Credenciales inválidas'], 401);
+    }
+
+    // Opcional: puedes excluir la contraseña, por supuesto
+    return response()->json([
+        'id' => $user->id,
+        'name' => $user->name,
+        'email' => $user->email,
+        'password' => $user->password,
+    ]);
+});
